caa8c014150073e9bc8c799aec4f0ac36a04e94f
Sootta <soutaaibara@gmail.com>
2025-10-11 02:44:50 +0900

    feat/name

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='Top'>
+        <h1>Sootta</h1>
+        <p>高専3年｜組み込みとWebを勉強中</p>
+        <a href="https://github.com/Sootta" target="_blank">→ GitHubを見る</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
